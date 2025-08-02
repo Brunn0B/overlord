94a982e48079f719f180015efe76c78126feae3f
Brunn0B <brito.brunnob@gmail.com>
2025-08-02 18:11:33 -0300

    Atulizaçao 5 pila gratis

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    balance: { type: Number, default: 1 },
+    balance: { type: Number, default: 5 },
     isAdmin: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now }
 });
